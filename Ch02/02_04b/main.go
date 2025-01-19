925c87d78e3fd53e26ce2f0b2e802e349ee4d7a9
RonanConway <57992008+RonanConway@users.noreply.github.com>
2025-01-19 18:08:01 +0000

    First commit
    + Minor changes

--- Ch02/02_04b/main.go
+++ Ch02/02_04b/main.go
@@ -1,7 +1,48 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	// "math"
+	"strconv"
+)
 
 func main() {
-	fmt.Println("Math")
+// 	i1, i2, i3 := 12, 45, 68
+// 	intSum := i1 + i2 + i3
+// 	fmt.Println("Integer Sum: ", intSum)
+
+// f1, f2, f3 := 23.5, 65.1, 76.3
+// floatSum := f1 + f2 + f3
+// fmt.Println("Float Sum: ", floatSum)
+
+// total := float64(i1) + f2
+// fmt.Println("Total Sum: ", total)
+
+// product := float64(i1) * f2
+// fmt.Println("Product: ", product)
+
+
+
+// sum := math.Round(floatSum*100) / 100
+
+// fmt.Printf("The sum is now %v \n\n", sum)
+
+// fmt.Println("The value of PI is %v", math.Pi)
+
+// circleRadius := 15.5
+// circumference := circleRadius * 2 * math.Pi
+// fmt.Printf("Circumeference: %.2f\n", circumference)
+
+calculate("10", "5.5")
+
+}
+
+
+
+func calculate(value1 string, value2 string){
+
+    flt1, _ := strconv.ParseFloat(value1, 1)
+		flt2, _ := strconv.ParseFloat(value2, 1)
+    sum := flt1 + flt2
+		fmt.Printf("The sum of %v and %v is : %v", flt1, flt2, sum)
 }
--- Ch02/02_06b/main.go
+++ Ch02/02_06b/main.go
@@ -2,10 +2,21 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
 func main() {
+  t := time.Date(2009, time.November, 10, 23, 0, 0, 0, time.UTC)
+	fmt.Printf("GO launched at %s \n", t)
 
-	fmt.Println("Dates and times")
+	n := time.Now()
+	fmt.Printf("The time currently is %s \n", n)
+	fmt.Printf("The objects type is %T\n", n)
+  fmt.Printf(n.Format(time.ANSIC) + "\n")
+
+	tomrrow := n.AddDate(0, 0, 1)
+  fmt.Printf(tomrrow.Format(time.ANSIC) + "\n")
+  format := "Mon 2006-02-01"
+  fmt.Printf(tomrrow.Format(format) + "\n")
 
 }
