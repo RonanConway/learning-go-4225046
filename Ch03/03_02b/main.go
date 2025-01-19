5d740775c552d9b3d98cc0e5fe6027ae3bbd6354
RonanConway <57992008+RonanConway@users.noreply.github.com>
2025-01-19 18:30:37 +0000

    Chapter 3

--- Ch03/03_02b/main.go
+++ Ch03/03_02b/main.go
@@ -5,5 +5,21 @@ import (
 )
 
 func main() {
-	fmt.Println("Pointers")
+	
+	anInt := 42
+	var p *int = &anInt
+
+	if p == nil{
+		fmt.Println(" p is nil")
+	} else {
+	  fmt.Println("Value of P : ", *p)
+	}
+
+
+	value1 :=42.13
+	pointer := &value1
+	*pointer = *pointer / 31
+	fmt.Println("Value 1 : ", *pointer)
+	fmt.Println("Original Value : ", value1)
+
 }
--- Ch03/03_03b/main.go
+++ Ch03/03_03b/main.go
@@ -2,8 +2,37 @@ package main
 
 import (
 	"fmt"
+	"sort"
 )
 
 func main() {
-	fmt.Println("Arrays")
+	var colors [3]string
+	colors[0] = "Red"
+	colors[1] = "Green"
+	colors[2] = "Blue"
+	fmt.Println(colors)
+	fmt.Println(colors[1])
+
+  var numbers =[5]int{5,3,1,2,4}
+	fmt.Println(numbers)
+	fmt.Println("Number of numbers: ", len(numbers))
+
+	var newcolors = make([]string, 0, 3)
+  newcolors = append(newcolors, "Indigo", "Violet", "Puce")
+	fmt.Println(newcolors)
+
+	newcolors = append(newcolors, "Black", "Purple")
+	fmt.Println(newcolors)
+
+  newcolors = remove(newcolors, 0)
+	fmt.Println(newcolors)
+
+	sort.Strings(newcolors)
+	fmt.Println(newcolors)
+
+}
+
+
+func remove(slice [] string, i int) [] string{
+	return append(slice[:i], slice[i+1:]...)
 }
